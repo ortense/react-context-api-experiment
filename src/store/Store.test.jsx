9c30846d8fb60d8151d1c3751f1a1ae684835af1
Marcus Ortense <marcus.ortense@outlook.com>
2019-09-06 12:54:49 -0300

    add unit tests for Store methods

--- src/store/Store.test.jsx
+++ src/store/Store.test.jsx
@@ -1,14 +1,58 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {configure, shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import {Store} from './Store'
 
+// this is necessary because create-react-app doesn't allow jest setup files
+configure({ adapter: new Adapter() })
+
 describe('Store', () => {
+  const FakeApp = () => <h1>Fake</h1>
+
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    const Fake = () => <h1>Fake</h1>
-    ReactDOM.render(<Store><Fake/></Store>, div)
+    ReactDOM.render(<Store><FakeApp/></Store>, div)
     ReactDOM.unmountComponentAtNode(div)
   })
+
+  describe('dispatch', () => {
+    const fakeReducers = { test: jest.fn() }
+    const initialState = {}
+    const fakeAction = { type: 'test', payload: { test: true } }
+    const wrapper = shallow(
+      <Store reducers={fakeReducers} initialState={initialState}>
+        <FakeApp/>
+      </Store>
+    )
+    const store = wrapper.instance()
+
+    afterEach(() => {
+      fakeReducers.test.mockClear()
+      wrapper.setState(initialState)
+    })
+
+    it('should be a function', () => {
+      expect(typeof store.dispatch).toBe('function')
+    })
+
+    it('should call reducers by action type', () => {
+      store.dispatch(fakeAction)
+      expect(fakeReducers.test).toHaveBeenCalledTimes(1)
+      expect(fakeReducers.test).toHaveBeenCalledWith(initialState, fakeAction.payload)
+    })
+  })
+
+  describe('connect', () => {
+    it('should be a static method', () => {
+      expect(typeof Store.connect).toBe('function')
+    })
+
+    it('should return a valid component', () => {
+      const ConectedComponent = Store.connect(FakeApp)
+      const div = document.createElement('div')
+      ReactDOM.render(<ConectedComponent/>, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+  })
 })
-
-
